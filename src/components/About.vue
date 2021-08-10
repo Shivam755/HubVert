90a11134d786ae84ec20468858c5594515be8ec3
joyotea <jyotighosh101@gmail.com>
2021-08-10 20:30:17 +0530

    About us

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,80 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-section">
+  <h1>About Us Page</h1>
+  <p>Some text about who we are and what we do.</p>
+</div>
+<h2>xyz</h2>
+	<div class="card">
+  <img src="img_avatar.png" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Shivam Gupta</b></h4> 
+    <p>CEO</p> 
+    <p> Contact : www.shivam567@gmail.com </p>
+
   </div>
+</div>
+
+<br>
+<div class="card">
+  <img src="img_avatar2.png" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jyoti Ghosh</b></h4> 
+    <p>Founder</p> 
+    <p> Contact : jyotighosh101@gmail.com </p>
+  </div>
+</div>
+
+<br>
+
+<div class="card">
+  <img src="img_avatar.png" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Tejas Pophale</b></h4> 
+    <p>Co-Founder</p> 
+    <p> Contact : tejaspophale01@gmail.com </p>
+  </div>
+</div>
+
+<br>
+
+<div class="card">
+  <img src="img_avatar2.png" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Shrutika Gholap</b></h4> 
+    <p>Manager</p> 
+    <p> Contact : shrutika2002@gmail.com </p>
+  </div>
+</div>
+
+<br> 
+
+<div class="card">
+  <img src="img_avatar.png" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Harshal Gondkar</b></h4> 
+    <p>Employee/Clerk</p> 
+    <p> Contact : harshal26092002@gmail.com  </p>
+  </div>
+</div>
 </template>
+
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+  border-radius: 5px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+img {
+  border-radius: 5px 5px 0 0;
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
