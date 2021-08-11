7b1c5a19f4b31fc32df1311bea6ec7a354ae36c2
Shivam Gupta <66762938+Shivam755@users.noreply.github.com>
2021-08-11 16:22:01 +0530

    adding changes in profile

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -66,14 +66,7 @@ export default {
 
 
 <style>
-<<<<<<< HEAD
 #profilePic{
     border-radius: 50%;
 }
-=======
-  #profilePic
-  {
-  border-radius: 50%;
-  }
->>>>>>> 9b85291fbb4aca5b1dd420373a8ace97deb0a805
 </style>
