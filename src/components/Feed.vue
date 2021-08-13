8e4ea416a82673825acdbd872ba732a967e9a7a9
hrshl13 <harshal26092002@gmail.com>
2021-08-13 19:00:57 +0530

    created methods for photos, videos and quotes

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,12 +1,12 @@
 <template>
     <h1><b><font color="orange">MOOD</font></b></h1>
-    <img src="https://th.bing.com/th/id/OIP.bjEc692svnnjSPoWamxjeAHaHa?w=180&h=180&c=7&o=5&dpr=1.25&pid=1.7" alt="team member" width="100" height="100">
+    <img class="mood" src="https://th.bing.com/th/id/OIP.bjEc692svnnjSPoWamxjeAHaHa?w=180&h=180&c=7&o=5&dpr=1.25&pid=1.7" alt="team member" width="100" height="100">
     <input type="radio" id="angry" name="mood" value="ANGRY"><label for="SAD">SAD</label>&emsp;
-    <img src="https://th.bing.com/th/id/OIP.k4e2qw39PCpvMZdV3C6iVAHaKz?w=126&h=184&c=7&o=5&dpr=1.25&pid=1.7" alt="team member" width="100" height="100">
+    <img class="mood" src="https://th.bing.com/th/id/OIP.k4e2qw39PCpvMZdV3C6iVAHaKz?w=126&h=184&c=7&o=5&dpr=1.25&pid=1.7" alt="team member" width="100" height="100">
     <input type="radio" id="happy" name="mood" value="HAPPY"><label for="ANGRY">ANGRY</label>&emsp;
-    <img src="https://th.bing.com/th/id/OIP.iADgkyH2pg4yb2Jg5zSNugHaHa?w=204&h=204&c=7&o=5&dpr=1.25&pid=1.7" alt="team member" width="100" height="100"/>
+    <img class="mood" src="https://th.bing.com/th/id/OIP.iADgkyH2pg4yb2Jg5zSNugHaHa?w=204&h=204&c=7&o=5&dpr=1.25&pid=1.7" alt="team member" width="100" height="100"/>
     <input type="radio" id="tired" name="mood" value="TIRED"><label for="HAPPY">HAPPY</label>&emsp;
-    <img src="https://th.bing.com/th/id/OIP.bCbUtGyUdw4KFnH18zVz1QHaHa?w=198&h=198&c=7&o=5&dpr=1.25&pid=1.7" alt="team member" width="100" height="100">
+    <img class="mood" src="https://th.bing.com/th/id/OIP.bCbUtGyUdw4KFnH18zVz1QHaHa?w=198&h=198&c=7&o=5&dpr=1.25&pid=1.7" alt="team member" width="100" height="100">
     <input type="radio" id="sad" name="mood" value="SAD"><label for="TIRED">TIRED</label>&emsp;
     <!-- <form action=""> -->
 
@@ -22,12 +22,18 @@
     <label for="SING">SING</label>
     <h1><b><font color="orange">FEED</font></b></h1>
     <input type="text" name="word" v-model="word">&emsp;
-    <button type="submit" @click.prevent="videoSearch()">Search</button>
+    <button type="submit" @click.prevent="Search()">Search</button><br>
+    <div>
+        <a href="" @click.prevent="quoteSearch()">Quotes</a> | 
+        <a href="" @click.prevent="photoSearch()">Images</a> | 
+        <a href="" @click.prevent="videoSearch()">Videos</a>
+    </div>
+    
     <hr>
-    <div class="container"></div>
+    <div class="container" v-html="photoSearch()"></div>
 </template>
 <style>
-    img{
+    .mood{
         border-radius: 50%;
     }
     h1{
@@ -36,28 +42,11 @@
 </style>
 
 
-// // var word="sad";
-//         fetch("https://quotable.io/quotes?limit=100")
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log(data)
-//                 data.results.forEach(result => {
-//                     document.writeln(`<b><i>`+result.content+`</i></b>`+" -"+`<b><br>`+result.author+`</b><br>`);
-//                 });
-//             })
-// export default ({
-//     data:()=>{
-//         return{
-//             search:''
-//         }
-//     },
-//     methods
-// })
 
 <script>
 import $ from 'jquery'
-    const key="AIzaSyCGGV6g7Uh_aFD9C-nC9o7S8bj5Kzj6g0M";
-    // var video='';
+const key="AIzaSyCGGV6g7Uh_aFD9C-nC9o7S8bj5Kzj6g0M";
+var video='';
     // let i;
 export default ({
     data:()=> {
@@ -66,26 +55,49 @@ export default ({
         }
     },
     methods:{
-        // searchthis: function () {
-        //     console.log(this.word);
-        //     if(this.word.trim()!==''){
-        //         fetch("https://quotable.io/quotes?limit=100")
-        //             .then(response => response.json())
-        //             .then(data => {
-        //                 console.log(data)
-        //             }
-        //         )
-        //     }
-        // },
+        Search: function(){
+
+        },
+        quoteSearch: function () {
+            console.log(this.word);
+            if(this.word.trim()!==''){
+                fetch("https://quotable.io/quotes?limit=100")
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                    }
+                )
+            }
+        },
         videoSearch: function(){
             $.get("https://www.googleapis.com/youtube/v3/search?key="+key+"&type=video&part=snippet&maxResults=10&q="+this.word,function(data){
                 console.log(data);
-            data.items.forEach(item => {
-                video = `
-                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
-                `
-                $(".container").append(video)
-        });
-    });
-}}})
+                data.items.forEach(item => {
+                    video = `
+                    <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                    `
+                    $(".container").append(video)
+                });
+            });
+        },
+        photoSearch: function(){
+            fetch("https://api.pexels.com/v1/search?query="+this.word,{
+        headers: {
+            Authorization: "563492ad6f917000010000011da5f68fc8c545dc89d3186b2631afc8"
+        }
+        })
+        .then(resp => {
+            return resp.json()
+        })
+        .then(data => {
+            var images = data.photos;
+            data.photos.forEach(image =>{
+                images = `<img src=${image.src.tiny} />`
+                $(".container").append(images)
+            })
+            console.log(data);
+        })
+        }
+        },
+})
 </script>
